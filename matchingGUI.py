3f2f578874e7cd05dc43119e6da645334264e56c
zilmara <zilmara@vt.edu>
2020-05-01 18:48:36 -0400

    This commit includes a funtional GUI with all the necessary images

--- matchingGUI.py
+++ matchingGUI.py
@@ -3,95 +3,249 @@
 import os
 import pygame
 import pyglet
+import random
 import tkinter
 import tkinter.messagebox
 import tkinter.font
 import tkinter.ttk
+from string import Template
 from tkinter.ttk import Frame, Button, Style
 from random import randrange
 from gtts import gTTS
 
-def helloCallBack():
-    tkinter.messagebox.showinfo( "Hello Python", "Hello World")
+count = 0
+randomNumber = 0
 
-def SoundNumbers():
-        
-    #button1.pack()
+def doneCallBack():
+    tkinter.messagebox.showinfo( "Matching Game", "GOOD JOB!!!")
+    return 0;
+
+def pressedOne():
+    global one
+    one = True
+    randomNumbers(1)
+
+def pressedTwo():
+    global two
+    two = True
+    randomNumbers(2)
+
+def pressedThree():
+    global three
+    three = True
+    randomNumbers(3)
+	
+def pressedFour():
+    global four
+    four = True
+    randomNumbers(4)
+
+def pressedFive():
+    global five
+    five = True
+    randomNumbers(5)
+
+def pressedSix():
+    global six
+    six = True
+    randomNumbers(6)	
+
+def pressedSeven():
+    global seven
+    seven = True
+    randomNumbers(7)
+	
+def pressedEight():
+    global eight
+    eight = True
+    randomNumbers(8)
+	
+def pressedNine():
+    global nine
+    nine = True
+    randomNumbers(9)
+	
+def pressedTen():
+    global ten
+    ten = True
+    randomNumbers(10)	
+	
+def checkCorrectness(numberPressed, randomNumber):
+    
     language = 'en'
-    print(randrange(1,11))
-    myText = "Hello Zilmara!"
+    
+    if (numberPressed == randomNumber):
+        myText = "Correct!"
+        global count
+        count = count + 1
+    else:
+        myText = "Incorrect!"
+		
+    myobj = gTTS(text=myText, lang=language, slow=False) 
+      
+    myobj.save(myText + ".mp3") 
 
-    helv60 = tkinter.font.Font(family='Helvetica', size=10, weight=tkinter.font.BOLD)
+    pygame.mixer.init()
+    pygame.mixer.music.load(myText + ".mp3")
+    pygame.mixer.music.play()
 
+def wordNum(randomNumber):
+    if (randomNumber == 1):
+        myText = "one"
+    elif (randomNumber == 2):
+        myText = "two"
+    elif (randomNumber == 3):
+        myText = "three"
+    elif (randomNumber == 4):
+        myText = "four"
+    elif (randomNumber == 5):
+        myText = "five"
+    elif (randomNumber == 6):
+        myText = "six"
+    elif (randomNumber == 7):
+        myText = "seven"
+    elif (randomNumber == 8):
+        myText = "eight"
+    elif (randomNumber == 9):
+        myText = "nine"
+    elif (randomNumber == 10):
+        myText = "ten"	
+		
+    return myText
+	
+def startNumbers():
+
+    language = 'en'
+	#Start with first random number
+    global randomNumber
+    randomNumber = randrange(1,11)
+	
+    myText = wordNum(randomNumber)
+		
     myobj = gTTS(text=myText, lang=language, slow=False) 
       
     # Saving the converted audio in a mp3 file named 
-    # welcome  
-    myobj.save("welcome1.mp3") 
-
+    myobj.save(myText + ".mp3") 
+    #read first random number
     pygame.mixer.init()
-    pygame.mixer.music.load("welcome.mp3")
-    pygame.mixer.music.play()
+    pygame.mixer.music.load(myText+ ".mp3")
+    pygame.mixer.music.play()	
+	
+def randomNumbers(numberPressed):
+    
+    global randomNumber
+    checkCorrectness(numberPressed, randomNumber)
 
+    language = 'en'
+    randomNumber = randrange(1,11)
+    if (randomNumber == 1):
+        myText = "one"
+    elif (randomNumber == 2):
+        myText = "two"
+    elif (randomNumber == 3):
+        myText = "three"
+    elif (randomNumber == 4):
+        myText = "four"
+    elif (randomNumber == 5):
+        myText = "five"
+    elif (randomNumber == 6):
+        myText = "six"
+    elif (randomNumber == 7):
+        myText = "seven"
+    elif (randomNumber == 8):
+        myText = "eight"
+    elif (randomNumber == 9):
+        myText = "nine"
+    elif (randomNumber == 10):
+        myText = "ten"
+	
+    if (count == 20):
+        doneCallBack()
+    else:
+        myobj = gTTS(text=myText, lang=language, slow=False) 
+        filename = myText + ".mp3"
+
+        myobj.save(filename) 
+	
+        pygame.mixer.init()
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play()
 
 def main():
 
     root = tkinter.Tk()
+    root.config(cursor=('circle'))
     # Add Label to the root window 
     tkinter.Label(root, text = 'Learning Numbers', font =('Verdana', 15)).pack(pady = 10) 
 	
     #Set up photo images for GUI
-    photo1 = tkinter.PhotoImage(file = "red1.png") 
+    #photo1 = tkinter.PhotoImage(file = "red1.png") 
+    photo1 = tkinter.PhotoImage(file = "1092116.png")
     photo2 = tkinter.PhotoImage(file = "blue2.png") 
-    photo3 = tkinter.PhotoImage(file = "black3.png") 
+    #photo3 = tkinter.PhotoImage(file = "black3.png") 
+    photo3 = tkinter.PhotoImage(file = "2041976.png")
     photo4 = tkinter.PhotoImage(file = "purple4.png") 
-    photo5 = tkinter.PhotoImage(file = "red5.png") 
+    #photo5 = tkinter.PhotoImage(file = "red5.png") 
+    photo5 = tkinter.PhotoImage(file = "1803800.png")
     photo6 = tkinter.PhotoImage(file = "purple6.png")
-    photo7 = tkinter.PhotoImage(file = "teal7.png") 
+    #photo7 = tkinter.PhotoImage(file = "teal7.png") 
+    photo7 = tkinter.PhotoImage(file = "675119.png")
     photo8 = tkinter.PhotoImage(file = "gray8.png") 
-    photo9 = tkinter.PhotoImage(file = "red9.png") 
+    #photo9 = tkinter.PhotoImage(file = "red9.png") 
+    photo9 = tkinter.PhotoImage(file = "numNine.png")
     photo10 = tkinter.PhotoImage(file = "gold10.png") 
     startPhoto = tkinter.PhotoImage(file = "start.png")
     
     # Resizing image to fit on button 
-    photo1 = photo1.zoom(2) 
-    photo1 = photo1.subsample(10,10) 
+    photo1 = photo1.zoom(1) 
+    photo1 = photo1.subsample(8,8) 
     photo2 = photo2.zoom(3) 
     photo2 = photo2.subsample(11,11) 
-    photo3 = photo3.zoom(3) 
-    photo3 = photo3.subsample(11, 11)
+    photo3 = photo3.zoom(1) 
+    photo3 = photo3.subsample(8, 8)
     photo4 = photo4.zoom(3)	
     photo4 = photo4.subsample(11, 11) 
-    photo5 = photo5.zoom(3)
-    photo5 = photo5.subsample(11, 11) 
+    photo5 = photo5.zoom(1)
+    photo5 = photo5.subsample(8, 8) 
     photo6 = photo6.zoom(3)
     photo6 = photo6.subsample(11, 11)
-    photo7 = photo7.zoom(8)	
-    photo7 = photo7.subsample(11, 11)
+    photo7 = photo7.zoom(1)	
+    photo7 = photo7.subsample(8, 8)
     photo8 = photo8.zoom(8)	
     photo8 = photo8.subsample(11, 11)
-    photo9 = photo9.zoom(3)	
-    photo9 = photo9.subsample(11, 11)
+    photo9 = photo9.zoom(1)	
+    photo9 = photo9.subsample(8, 8)
     photo10 = photo10.zoom(10)	
     photo10 = photo10.subsample(11, 11)
     startPhoto = startPhoto.zoom(2)
     startPhoto = startPhoto.subsample(11, 11)
 
-    #Set image on button
-    tkinter.Button(root, text = 'Click Me !', command=SoundNumbers, image = photo1).place(x=50, y=150)
-    tkinter.Button(root, text = 'Click Me !', image = photo2).place(x=200, y=100)
-    tkinter.Button(root, text = 'Click Me !', image = photo3).place(x=400, y=50)
-    tkinter.Button(root, text = 'Click Me !', image = photo4).place(x=600, y=100)
-    tkinter.Button(root, text = 'Click Me !', image = photo5).place(x=800, y=150)
-    tkinter.Button(root, text = 'Click Me !', image = photo6).place(x=30, y=400)
-    tkinter.Button(root, text = 'Click Me !', image = photo7).place(x=220, y=450)
-    tkinter.Button(root, text = 'Click Me !', image = photo8).place(x=400, y=500)
-    tkinter.Button(root, text = 'Click Me !', image = photo9).place(x=580, y=450)
-    tkinter.Button(root, text = 'Click Me !', image = photo10).place(x=780, y=400)
-    
-	#Start button
-    tkinter.Button(root, text = 'Let\'s Begin!', image = startPhoto).place(x=440, y=330)
+	#Set image on button
+    tkinter.Button(root, text = 'Click Me !', command=pressedOne,
+					image = photo1, highlightcolor="cyan").place(x=35, y=150)
+    tkinter.Button(root, text = 'Click Me !', command=pressedTwo, 
+					image = photo2).place(x=200, y=100)
+    tkinter.Button(root, text = 'Click Me !', command=pressedThree,
+					image = photo3).place(x=390, y=50)
+    tkinter.Button(root, text = 'Click Me !', command=pressedFour,
+					image = photo4).place(x=600, y=100)
+    tkinter.Button(root, text = 'Click Me !', command=pressedFive, 
+					image = photo5).place(x=800, y=150)
+    tkinter.Button(root, text = 'Click Me !', command=pressedSix, 
+					image = photo6).place(x=30, y=400)
+    tkinter.Button(root, text = 'Click Me !', command=pressedSeven, 
+					image = photo7).place(x=210, y=450)
+    tkinter.Button(root, text = 'Click Me !', command=pressedEight, 
+					image = photo8).place(x=400, y=500)
+    tkinter.Button(root, text = 'Click Me !', command=pressedNine, 
+					image = photo9).place(x=580, y=450)
+    tkinter.Button(root, text = 'Click Me !', command=pressedTen, 
+					image = photo10).place(x=780, y=400)
 	
+	#Start button
+    tkinter.Button(root, text = 'Let\'s Begin!', command=startNumbers, 
+					image = startPhoto).place(x=440, y=330)	
+					
     root.wm_title("Matching Numbers")
     root.geometry("1000x750")
     root.mainloop()
